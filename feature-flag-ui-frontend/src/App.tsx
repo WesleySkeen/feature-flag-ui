3ba5b6a26308365e4ccf6491a06914592340fabb
Wesley Skeen <wesley.skeen@emailage.com>
2025-04-09 14:51:51 +0100

    replace logo with loading spinner and update app layout

--- feature-flag-ui-frontend/src/App.tsx
+++ feature-flag-ui-frontend/src/App.tsx
@@ -1,20 +1,14 @@
-import reactLogo from './assets/react.svg'
+import loading_spinner from './assets/loading-spinner.svg'
 import './App.css'
 
 function App() {
-  return (
-    <>
-      <div>
-          <img src={reactLogo} className="logo react" alt="React logo" />
-      </div>
-      <div className="card">
-          <h1>Loading Flags</h1>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+    return (
+        <>
+            <img src={loading_spinner} className="loading-spinner" alt="loading feature flags logo"/>
+            <h1>Loading Feature Flags</h1>
+        </>
+    )
 }
 
 export default App
